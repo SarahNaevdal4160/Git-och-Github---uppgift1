b9baf8b81aa4302086bc3d713d8339a75298297f
Sarah Naevdal <sarahnaevdal@Sarahs-MacBook-Air.local>
2026-09-23 11:40:24 -0400

    uppdaterade main-filen

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej från verkstaden");
+        System.out.println("Hej från Virginia Beach, VA, USA!");
+        //Denna texten har ändrats 23 sept. 2026.
     }
 }
