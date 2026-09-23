8dd1681def97dc584cdd139e1d86e8e885818ed3
Sarah Naevdal <sarahnaevdal@Sarahs-MacBook-Air.local>
2026-09-23 12:30:02 -0400

    Jag lade till mitt fornamn i halsningen.

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej från Virginia Beach, VA, USA!");
+        System.out.println("Hej från Virginia Beach, VA, USA! /Sarah");
         //Denna texten har ändrats 23 sept. 2026.
     }
 }
